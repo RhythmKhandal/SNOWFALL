0dd2e8056f0d5324681f73b85c1142d5f53ec3ea
RhythmKhandal <73952591+RhythmKhandal@users.noreply.github.com>
2021-04-19 15:02:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function preload(){
+backgroundImg=loadImage("snow1.jpg")
+
+}
+
+function setup() {
+  createCanvas(1300,600);
+ // createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(backgroundImg);  
+  //drawSprites();
+}
